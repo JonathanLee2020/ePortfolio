262e8a4e19853ef646fdfcbf45f800fd69a3d434
Jonathan <jonathanlee882@gmail.com>
2023-04-11 14:21:34 +1000

    added contact class

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // template_2yn8mx5
+// service_xvi7dk6
+// public key: AFYPuaJKIMP26vHVm
+
+// <script type="text/javascript"
+//         src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js">
+// </script>
+// <script type="text/javascript">
+//    (function(){
+//       emailjs.init("AFYPuaJKIMP26vHVm");
+//    })();
+// </script>
+
+function contact () {
+    console.log("it worked");
+}

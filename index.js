c690fccbcbf0e560bdc8eece1ec95a800cccac79
Jonathan <jonathanlee882@gmail.com>
2023-04-11 19:45:29 +1000

    completed fully functioning modal

--- index.js
+++ index.js
@@ -15,20 +15,33 @@ function contact () {
     event.preventDefault();
     const loading = document.querySelector(".modal__overlay--loading");
     const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+
+    emailjs
+        .sendForm(
+            "service_xvi7dk6",
+            "template_2yn8mx5",
+            event.target,
+            "AFYPuaJKIMP26vHVm"
+
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible"
+            console.log("this worked again")
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert (
+                "the email service is temporarily unavailable. Please contact me directly on jonathanlee882@gmail.com"
+            );
+        })
+
+
+
+
     loading.classList += " modal__overlay--visible"
     setTimeout(() => {
         loading.classList.remove("modal__overlay--visible");
         success.classList += " modal__overlay--visible"
     }, 1000)
 }
-    // emailjs
-//         .sendForm(
-//             "service_xvi7dk6",
-//             "template_2yn8mx5",
-//             event.target,
-//             "AFYPuaJKIMP26vHVm"
-
-//         ).then(() => {
-//             console.log("this worked again")
-//         })
-// }
+    

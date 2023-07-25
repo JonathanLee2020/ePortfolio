d1bf4c6205d80d4fd63919596e4becb48b73699f
Jonathan Lee <jonathanlee882@gmail.com>
2023-07-25 21:23:05 +1000

    added fade in for shapes

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ function moveBackground(event) {
   for (let i = 0; i < shapes.length; ++i) {
     const isOdd = i % 2 !== 0;
     const boolInt = isOdd ? -1 : 1;
+    console.log(boolInt)
     // Added rotate after tutorial
     shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
   }
 }
+
 
 let contrastToggle = false;
 // document.body.classList += " hello";
